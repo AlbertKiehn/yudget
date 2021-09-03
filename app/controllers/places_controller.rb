e62d98aa1cc70f10ccf15bc2c698b649ee635734
Johannes von Foerster <jvonfoerster@gmail.com>
2021-09-03 10:51:23 +0100

    indexfix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,6 +4,13 @@ class PlacesController < ApplicationController
     @location = params[:spot]
     placesold = Place.where(category: params[:category])
     @places = placesold.near(@location, 8)
+    Cloudinary.config do |config|
+      config.cloud_name = 'mk01'
+      config.api_key = '243339615997478'
+      config.api_secret = 'YEyzNmKecdmScbJTs5wcY3KFlO8'
+      config.secure = true
+      config.cdn_subdomain = true
+    end
   end
 
   def show
