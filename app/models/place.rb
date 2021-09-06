c9c9cbfefe3ae9ef8fc50c00c72103f07ca51f84
Albert Kiehn <kiehn.albert@icloud.com>
2021-09-06 16:37:55 +0100

    amazing stuff

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,6 +4,11 @@ class Place < ApplicationRecord
 
   validates :name, :category, :description, :summary, :address, presence: true
   validates :address, uniqueness: true
+  validates :description, length: { minimum: 200 }
+  validates :description, length: { maximum: 405 }
+  validates :summary, length: { minimum: 10 }
+  validates :summary, length: { maximum: 100 }
+  validates :photo, presence: true
 
   acts_as_votable
 
